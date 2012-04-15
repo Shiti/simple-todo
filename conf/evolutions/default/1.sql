0f6526829111790c6986c914d9f08da9ab8f7de4
Rohit <rohit@tingendab.com>
2012-04-15 20:06:08 +0530

    Fixing evolution scripts

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,9 +1,13 @@
--- Todos SCHEMA
+# Todos SCHEMA
 
+# --- !Ups
 CREATE TABLE Todo (
-  id VARCHAR(255) PRIMARY KEY ,
+  id VARCHAR(255) PRIMARY KEY,
   text VARCHAR(100),
   done BOOLEAN,
   disp_order INT UNIQUE
 );
 
+
+# --- !Downs
+DROP TABLE Todo;
