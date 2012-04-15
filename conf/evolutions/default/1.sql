5a7f94f47a5f135233b5a593b64952b698668c11
Rohit <rohit@tingendab.com>
2012-04-15 19:39:26 +0530

    fixing the sql script. comment was giving error.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,6 +4,6 @@ CREATE TABLE Todo (
   id VARCHAR(255) PRIMARY KEY ,
   text VARCHAR(100),
   done BOOLEAN,
-  disp_order INT UNIQUE      --had to change the attribute name as its a reserved keyword
+  disp_order INT UNIQUE
 );
 
